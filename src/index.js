b6cb55461652a13a9639e0bc27a949dc27b6fba3
Dan <dansification@gmail.com>
2021-04-02 10:55:27 -0400

    Be able to execute SonarQube command

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const { context, getOctokit } = require('@actions/github')
 const exec = require('@actions/exec')
 const axios = require('axios').default
 
-const sonarQubeInfo = (repo) =>  ({
+const sonarQubeConfig = (repo) =>  ({
 	projectKey: getInput('projectKey') ? getInput('projectKey') : `${repo.owner}-${repo.repo}`,
 	projectName: getInput('projectName') ? getInput('projectName') : `${repo.owner}-${repo.repo}`,
 	projectBaseDir: getInput('projectBaseDir'),
@@ -13,3 +13,15 @@ const sonarQubeInfo = (repo) =>  ({
 
 const sonarQubeCommand = (sonarqube) =>
 	`sonar-scanner -Dsonar.projectKey=${sonarqube.projectKey} -Dsonar.projectName=${sonarqube.projectName} -Dsonar.sources=. -Dsonar.projectBaseDir=${sonarqube.projectBaseDir} -Dsonar.login=${sonarqube.token} -Dsonar.host.url=${sonarqube.host}`
+
+async function run () {
+	const repo = context.repo
+	const config = sonarQubeConfig(repo)
+
+	const scannerCommand = sonarQubeCommand(config)
+	await exec.exec(scannerCommand)
+
+	await new Promise(r => setTimeout(r, 5000))
+}
+
+run()
