e1308a6804c14c864372dd37fef2341a059413ff
Dan <dansification@gmail.com>
2021-04-02 10:25:10 -0400

    Extract info from action

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ const { getInput, info } = require('@actions/core')
 const { context, getOctokit } = require('@actions/github')
 const exec = require('@actions/exec')
 const axios = require('axios').default
+
+const sonarQubeInfo = (repo) =>  ({
+	projectKey: getInput('projectKey') ? getInput('projectKey') : `${repo.owner}-${repo.repo}`,
+	projectName: getInput('projectName') ? getInput('projectName') : `${repo.owner}-${repo.repo}`,
+	projectBaseDir: getInput('projectBaseDir'),
+	host: getInput('host'),
+	token: getInput('token')
+})
