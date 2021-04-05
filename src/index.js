8f776ae031c287795445d47ccc8d9c5694e8fbc7
Dan <dansification@gmail.com>
2021-04-05 10:58:28 -0400

    Be able to get SonarQube issues

--- src/index.js
+++ src/index.js
@@ -14,6 +14,29 @@ const sonarQubeConfig = (repo) =>  ({
 const sonarQubeCommand = (sonarqube) =>
 	`sonar-scanner -Dsonar.projectKey=${sonarqube.projectKey} -Dsonar.projectName=${sonarqube.projectName} -Dsonar.sources=. -Dsonar.projectBaseDir=${sonarqube.projectBaseDir} -Dsonar.login=${sonarqube.token} -Dsonar.host.url=${sonarqube.host}`
 
+const projectIssues = async (sonarqubeConfig, pageSize, page) => {
+  const tokenBase64 = Buffer.from(sonarqubeConfig.token + ':').toString('base64')
+
+  try {
+    const response = await axios.get(`${sonarqubeConfig.host}/api/issues/search?componentKeys=${sonarqubeConfig.projectKey}&statuses=OPEN&ps=${pageSize}&p=${page}`,
+      {
+        headers: {
+          Authorization: `Basic ${tokenBase64}`
+        }
+      }
+    )
+
+    const issues = response.data.issues
+    if (pageSize * page >= response.data.paging.total) {
+      return issues
+    }
+
+    return issues.concat(await projectIssues(sonarqubeConfig, pageSize, page + 1))
+  } catch(err) {
+    throw new Error('Error getting project issues from SonarQube. Please make sure you provided the host and token inputs.')
+  }
+}
+
 async function run () {
 	const repo = context.repo
 	const config = sonarQubeConfig(repo)
